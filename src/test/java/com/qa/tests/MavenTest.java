4706bde618e486362f959cf325c4423a7654a735
irma <kang403@gmail.com>
2018-12-06 15:00:19 -0500

    minor change

--- src/test/java/com/qa/tests/MavenTest.java
+++ src/test/java/com/qa/tests/MavenTest.java
@@ -7,7 +7,7 @@ public class MavenTest {
 
 	@Test
 	public void sum() {
-		System.out.println("SUM");
+		System.out.println("SUM - 1");
 		int a = 10;
 		int b = 20;
 		Assert.assertEquals(30,a+b);
@@ -15,7 +15,7 @@ public class MavenTest {
 
 	@Test
 	public void sub() {
-		System.out.println("SUB");
+		System.out.println("SUB - 1");
 		int a = 10;
 		int b = 20;
 		Assert.assertEquals(10,b-a);
